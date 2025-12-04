c36fb83fc82ef2fcdd58bb51a7855e5fb7de98de
isabellagilman <isabella.gilman002@gmail.com>
2025-12-04 03:23:24 +0000

    Implemented MaximalSuffix function

--- src/part1.cpp
+++ src/part1.cpp
@@ -4,6 +4,87 @@
 
 using namespace std;
 
+void MaximalSuffix(string pattern, bool reverseOrder,  int * maximalSuffix, int * period)
+{
+  *maximalSuffix = -1;
+  *period = 1;
+  int j = 0;
+  int k = 1;
+  int n = pattern.length();
+
+  while (j + k <= n)
+  {
+    int a = pattern[j + k];
+    int b = pattern[*maximalSuffix + k];
+
+    if (reverseOrder)
+    {
+      if (a < b)
+      {
+        j = j + k;
+        k = 1;
+        *period = j - *maximalSuffix;
+      }
+      else if (a == b)
+      {
+        if (k == *period)
+        {
+          j = j + *period;
+          k = 1;
+        }
+        else
+        {
+          k = k + 1;
+        }
+      }
+      else
+      {
+        *maximalSuffix = j;
+        j = *maximalSuffix + 1;
+        k = 1;
+        *period = 1;
+      }
+    }
+    else
+    {
+      if (a > b)
+      {
+        *maximalSuffix = j;
+        j = *maximalSuffix + 1;
+        k = 1;
+        *period = 1;
+      }
+      else if (a == b)
+      {
+        if (k == *period)
+        {
+          j = j + *period;
+          k = 1;
+        }
+        else
+        {
+          k = k + 1;
+        }
+      }
+      else
+      {
+        j = j + k;
+        k = 1;
+        *period = j - *maximalSuffix;
+      }
+    }
+  }
+}
+
+int TwoWayMatch(string pattern, string text)
+{
+  int len_pattern = pattern.length();
+  int l1, l2, p1, p2;
+  MaximalSuffix(pattern, false, &l1, &p1);
+  MaximalSuffix(pattern, true, &l2, &p2);
+
+}
+
 int main()
 {
   // TODO: Replace this line with actual logic
