c6ca49fc96cde7431c2e1c1abc7f683de58b209a
isabellagilman <isabella.gilman002@gmail.com>
2025-12-05 01:58:31 +0000

    Typed out Two-Way match alg according to paper

--- src/part1.cpp
+++ src/part1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "alg_graphs.h"
 
 using namespace std;
@@ -76,15 +77,90 @@ void MaximalSuffix(string pattern, bool reverseOrder,  int * maximalSuffix, int 
   }
 }
 
-int TwoWayMatch(string pattern, string text)
+vector<int> TwoWayMatch(string pattern, string text)
 {
+  vector<int> positions;
   int len_pattern = pattern.length();
-  int l1, l2, p1, p2;
+  int l, l1, l2, p, p1, p2, i, j;
   MaximalSuffix(pattern, false, &l1, &p1);
   MaximalSuffix(pattern, true, &l2, &p2);
-  cout << l1 << ", " << l2 << ", " << p1 << ", " << p2 << endl;
 
-  return 0;
+  if (l1 >= l2)
+  {
+    l = l1;
+    p = p1;
+  }
+  else
+  {
+    l = l2;
+    p = p2;
+  }
+
+  if (l < len_pattern/2 &&
+     (pattern.substr(0, l) == pattern.substr(l, l + p)))
+  {
+    int pos = 0;
+    int s = 0;
+    while (pos + len_pattern <= text.length())
+    {
+      i = max(l, s) + 1;
+      while (i <= len_pattern && pattern[i] == text[pos + i])
+      {
+        i = i + 1;
+      }
+      if (i <= len_pattern)
+      {
+        pos = pos + max(i - l, s - p + 1);
+        s = 0;
+      }
+      else
+      {
+        j = l;
+        while (j > s && pattern[j] == text[pos + j])
+        {
+          j = j - 1;
+        }
+        if (j <= s)
+        {
+          positions.push_back(pos);
+        }
+        pos = pos + p;
+        s = len_pattern - p;
+      }
+    }
+  }
+  else
+  {
+    int q = max(l, len_pattern - l) + 1;
+    int pos = 0;
+    while (pos + len_pattern <= text.length())
+    {
+      i = l + 1;
+      while (i <= len_pattern && pattern[i] == text[pos + i])
+      {
+        i = i + 1;
+      }
+      if (i <= len_pattern)
+      {
+        pos = pos + i - l;
+      }
+      else
+      {
+        j = l;
+        while (j > 0 && pattern[j] == text[pos + j])
+        {
+          j = j - 1;
+        }
+        if (j == 0)
+        {
+          positions.push_back(pos);
+        }
+        pos = pos + q;
+      }
+    }
+  }
+
+  return positions;
 }
 
 
